3cae3fbd63ecdeea1ee4b1f2da38656e4e5b87e9
osmanacikgoz-ysm <osmnacikgoz@outlook.com.tr>
2023-05-16 10:54:42 +0300

    first commit

---
+++ app/src/main/java/com/example/starwarsapp/network/ApiServices.kt
@@ -0,0 +1,20 @@
+package com.example.starwarsapp.network
+
+import com.example.starwarsapp.model.FilmResponse
+import com.example.starwarsapp.model.HomeWorldResponse
+import com.example.starwarsapp.model.PeopleResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.Url
+
+interface ApiServices {
+
+    @GET("people/?page/")
+    suspend fun getCharacters(@Query("page") page:Int): PeopleResponse
+
+    @GET
+    suspend fun getFilm(@Url url:String): FilmResponse
+
+    @GET
+    suspend fun getHomeWorld(@Url url:String): HomeWorldResponse
+}
