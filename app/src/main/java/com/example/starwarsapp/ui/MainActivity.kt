e368027901e6359ddc2b1bfe51a79bfb45f41068
osmanacikgoz-ysm <osmnacikgoz@outlook.com.tr>
2023-05-16 17:39:40 +0300

    navigation structure established

---
+++ app/src/main/java/com/example/starwarsapp/ui/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.starwarsapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.example.starwarsapp.R
+import com.example.starwarsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        appBarConfiguration = AppBarConfiguration.Builder(
+
+        ).build()
+
+        setSupportActionBar(binding.toolbarMain)
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
